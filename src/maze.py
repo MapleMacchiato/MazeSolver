2f40fb4cf967a0d7fed8ce130d6f3f54dc693b86
MapleMacchiato <gabriel.b.stein@gmail.com>
2024-06-04 19:28:46 -0400

    started working on maze class

---
+++ src/maze.py
@@ -0,0 +1,28 @@
+from graphics import Cell
+
+
+class Maze:
+
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        window,
+    ):
+
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.window = window
+
+        self.create_cells()
+
+    def create_cells(self):
+        pass
