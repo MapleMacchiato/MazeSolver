53971a14fc42454143aed2508675f765597fb732
MapleMacchiato <gabriel.b.stein@gmail.com>
2024-06-05 17:45:51 -0400

    updated graphics.py to include classes for points and lines

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(100, 100))
+    win.draw_line(line)
+
     win.wait_for_close()
 
 
